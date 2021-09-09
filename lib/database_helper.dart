064c23c43040e8bb98351b557ce0db3ce098230a
nj241098 <nisha.jain@grayopus.com>
2021-09-09 12:44:27 +0530

    All CRUD features are working.

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
@@ -26,11 +25,11 @@ class DatabaseHelper {
   _initiateDatabase() async {
     Directory directory = await getApplicationDocumentsDirectory();
     String path = join(directory.path, _dbName);
-    await openDatabase(path, version: _dbVersion, onCreate: _onCreate);
+    return await openDatabase(path, version: _dbVersion, onCreate: _onCreate);
   }
 
   Future _onCreate(Database db, int version) async {
-    db.query('''
+    db.execute('''
     CREATE TABLE $_tableName(
     $columnId INTEGER PRIMARY KEY,
     $columnName TEXT NOT NULL)
@@ -38,4 +37,27 @@ class DatabaseHelper {
     
     ''');
   }
+
+  Future<int> insert(Map<String, dynamic> row) async{
+    Database? db = await instance.database;
+    return await db!.insert(_tableName, row);
+  }
+
+  Future<List<Map<String, dynamic>>> queryAll() async{
+    Database? db = await instance.database;
+    return await db!.query(_tableName);
+  }
+
+  Future update(Map<String, dynamic> row) async{
+    Database? db = await instance.database;
+    int id = row[columnId];
+    return await db!.update(_tableName, row, where: '$columnId = ?', whereArgs: [id]);
+  }
+
+  Future<int> delete(int id) async{
+    Database? db = await instance.database;
+    return await db!.delete(_tableName, where: '$columnId = ?' , whereArgs: [id]);
+  }
 }
+
+  
